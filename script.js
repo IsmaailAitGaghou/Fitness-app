c261daeb4c1167c1ec9cedab52b711b51de64a0a
ismaail aitgaghou <62771021+rbbarnes123@users.noreply.github.com>
2022-08-24 03:00:16 +0100

    complete day3

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 const closeMenu = document.querySelector('.close-menu')
 const mobielNavigation = document.querySelector('.mobile-navigation')
-const overlay = document.querySelector('.overlay')
 const burgerIcon = document.querySelector('.toggle-menu')
+const overlay = document.querySelector('.overlay')
 
-burgerIcon.addEventListener('click', () => {
+
+
+function openMobileMenu() {
     mobielNavigation.classList.add('open')
-    openOverlay()
-})
+}
 
-closeMenu.addEventListener('click', () => {
+function closeMobileMenu() {
     mobielNavigation.classList.remove('open')
-    closeOverlay()
-})
+}
 
 function openOverlay() {
     overlay.classList.add('open')
@@ -20,3 +20,38 @@ function openOverlay() {
 function closeOverlay() {
     overlay.classList.remove('open')
 }
+
+overlay.addEventListener('click', function(e) {
+    if(e.currentTarget == e.target) {
+        closeMobileMenu()
+        closeOverlay()
+    }
+})
+
+burgerIcon.addEventListener('click', () => {
+    openMobileMenu()
+    openOverlay()
+})
+
+closeMenu.addEventListener('click', () => {
+    closeMobileMenu()
+    closeOverlay()
+})
+
+
+let observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
+        // go to some good part of video
+        vid.currentTime = 199.7;
+      } else {
+        if (entry.isIntersecting) {
+          vid.play();
+        } else {
+            vid.pause();
+        }
+      }
+    });
+  });
+  
+  observer.observe(vid);
